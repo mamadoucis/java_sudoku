d47e0ec0e68a49557b067d43d9a5d3ec911669cd
mamadoucis <108749999+mamadoucis@users.noreply.github.com>
2022-07-05 11:05:01 -0500

    Add files via upload

---
+++ sudokuproject/ColorDisplay.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ColorDisplay extends JFrame {
+    int prevBoard[][] = new int[9][9];
+    {
+        JButton changeColor = new JButton("Check");
+        changeColor.setBackground(Color.BLACK);
+        changeColor.setForeground(Color.GREEN);
+        changeColor.add(changeColor);
+    }
+}
---
+++ sudokuproject/Coordinates.java
@@ -0,0 +1,8 @@
+package com.company;
+public class Coordinates {
+    int x, y;
+    public Coordinates(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+}
